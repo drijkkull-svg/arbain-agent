61f76a8753d579bacdd8fea82f158904e34c2525
drijkkull-svg <drijkkull@gmail.com>
2026-05-06 08:10:46 +0700

    feat: tambah auto update check saat app dibuka

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:geolocator/geolocator.dart';
 import '../services/device_admin_service.dart';
+import '../services/auto_update_service.dart';
 import 'login_screen.dart';
 import 'pairing_screen.dart';
 import 'apps_screen.dart';
@@ -19,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final _auth = FirebaseAuth.instance;
   final _firestore = FirebaseFirestore.instance;
   final _deviceAdmin = DeviceAdminService();
+  final _autoUpdate = AutoUpdateService();
   String _status = 'Memulai...';
   bool _isTracking = false;
   bool _isRestricted = false;
@@ -33,6 +35,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _listenToDeviceCommands();
     _checkAdminStatus();
     _deviceAdmin.listenLockCommand();
+    WidgetsBinding.instance.addPostFrameCallback\((_\) => _autoUpdate.checkUpdate\(context\)\);
   }
 
   Future<void> _checkAdminStatus() async {
@@ -232,3 +235,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
