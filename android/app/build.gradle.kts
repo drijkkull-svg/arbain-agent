5fb9b6263951a9936b754d92a02f831f53866ba8
drijkkull-svg <drijkkull@gmail.com>
2026-05-06 23:45:51 +0700

    fix: tambah firebase dependencies di build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -45,3 +45,9 @@ android {
 flutter {
     source = "../.."
 }
+
+dependencies {
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+}
+
