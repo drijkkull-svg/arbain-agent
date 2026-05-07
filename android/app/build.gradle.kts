fd255b0469d2f1afa7113657e3e01cf9f04ac1b6
drijkkull-svg <drijkkull@gmail.com>
2026-05-07 09:29:34 +0700

    fix: tambah firebase dependencies untuk ForegroundService

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,3 +34,8 @@ android {
 flutter {
     source = "../.."
 }
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+}
