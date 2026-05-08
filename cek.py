e83e3880080c8c516afc005348bc7d791f283c32
drijkkull-svg <drijkkull@gmail.com>
2026-05-09 00:08:46 +0700

    feat: overlay JAM TIDUR modern dengan nama santri dan kamar

---
+++ cek.py
@@ -0,0 +1,5 @@
+﻿file = r"android\app\src\main\kotlin\com\example\arbain_agent\ArbainAccessibilityService.kt"
+with open(file, "r", encoding="utf-8") as f:
+    content = f.read()
+print("File berhasil dibaca!")
+print("Panjang:", len(content))
