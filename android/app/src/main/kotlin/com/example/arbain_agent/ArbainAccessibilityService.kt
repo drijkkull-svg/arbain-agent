ac794f3b131aba59acfce138b067189470656f88
drijkkull-svg <drijkkull@gmail.com>
2026-05-09 18:30:11 +0700

    revert: kembalikan overlay normal

--- android/app/src/main/kotlin/com/example/arbain_agent/ArbainAccessibilityService.kt
+++ android/app/src/main/kotlin/com/example/arbain_agent/ArbainAccessibilityService.kt
@@ -118,7 +118,7 @@ class ArbainAccessibilityService : AccessibilityService() {
         val wm = getSystemService(WINDOW_SERVICE) as android.view.WindowManager
         val params = android.view.WindowManager.LayoutParams(
             android.view.WindowManager.LayoutParams.MATCH_PARENT,
-            android.view.WindowManager.LayoutParams.MATCH_PARENT + 200,
+            android.view.WindowManager.LayoutParams.MATCH_PARENT,
             android.view.WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
             android.view.WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or android.view.WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN or android.view.WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS or android.view.WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
             android.graphics.PixelFormat.TRANSLUCENT
@@ -227,6 +227,7 @@ class ArbainAccessibilityService : AccessibilityService() {
 
     override fun onInterrupt() {}
 }
+
 
 
 
